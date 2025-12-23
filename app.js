aeb3ce64b35c021a556a53356bb9e8c44d0e5b09
gelm2mil <nerox2mil@proton.me>
2025-12-23 09:17:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,38 @@ console.log("✔ APP.js cargado correctamente — GELM 2026");
 function notificar(msg) {
     alert("📌 GELM:\n" + msg);
 }
+
+const canvas = document.getElementById("firmaCanvas");
+const ctx = canvas.getContext("2d");
+
+let firmando = false;
+
+function ajustarCanvas() {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+  ctx.lineWidth = 2;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "#000";
+}
+
+ajustarCanvas();
+window.addEventListener("resize", ajustarCanvas);
+
+canvas.addEventListener("pointerdown", e => {
+  firmando = true;
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener("pointermove", e => {
+  if (!firmando) return;
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+});
+
+canvas.addEventListener("pointerup", () => firmando = false);
+canvas.addEventListener("pointerleave", () => firmando = false);
+
+function limpiarFirma() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
